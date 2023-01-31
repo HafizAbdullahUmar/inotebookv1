d70176b0b8df410b1a237b07a2361d981404b3b0
Abdullah Umar <ksyan321@gmail.com>
2023-01-31 17:40:29 +0500

    tut 8

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,9 +6,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "ThisIsASuper$ecret";
 
-// Create a User using: POST "api/auth/". Doesn't require Auth
+// Create a User using: POST "api/auth/". No Login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
@@ -48,7 +48,50 @@ router.post(
       // res.json(user);
       res.json({ authToken });
     } catch (error) {
-      res.status(500).send("some error occured");
+      res.status(500).send("Internal Server Error Occured");
+    }
+  }
+);
+
+// Authenticate a User using: POST "api/auth/login". No Login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password Cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // If there are errors return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      res.status(500).send("Internal Server Error Occured");
     }
   }
 );
