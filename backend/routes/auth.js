c18d8e54429bd042f2af2c0a5958f133f688ed3c
Abdullah Umar <ksyan321@gmail.com>
2023-01-31 16:59:48 +0500

    tut 6

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,24 +13,30 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
-    console.log(req.body);
+  async (req, res) => {
+    // If there are errors return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        res.json({
-          error: "please enter a uique value for email",
-          message: err.message,
-        });
+
+    // Check wether the user with this email exist already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+      res.json(user);
+    } catch (error) {
+      res.status(500).send("some error occured");
+    }
   }
 );
 module.exports = router;
