3dee6399edea6d1bb4c5864e1ab1888fd94d127b
Abdullah Umar <ksyan321@gmail.com>
2023-02-02 18:24:28 +0500

    tut 18

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,7 @@ import React, { useContext } from "react";
 import NoteContext from "../context/notes/NoteContext";
 
 const About = () => {
-  const a = useContext(NoteContext);
-  a.update();
-  return (
-    <div>
-      This is About {a.state.name} who is in class {a.state.class}
-    </div>
-  );
+  return <div>This is About Page</div>;
 };
 
 export default About;
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const obj = {
-    name: "Abdullah",
-    class: "ICS(1st year)",
-  };
-  const [state, setState] = useState(obj);
-
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "Shehraz",
-        class: "ICS(1st year)",
-      });
-    }, 2000);
-  };
   return (
-    <NoteContext.Provider value={{ state, update }}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
   );
 };
 
